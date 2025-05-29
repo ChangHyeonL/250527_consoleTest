cecebc2a48a74520f9975f7f900db1f873e2ff27
Ch L <ckdgus48@gmail.com>
2025-05-29 11:10:08 +0900

    ClassStudy1 추가

---
+++ 250527_consoleTest/ClassStudy1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _250527_consoleTest
+{
+    internal class ClassStudy1
+    {
+    }
+}
